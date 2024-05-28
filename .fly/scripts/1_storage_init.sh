ff05a80f76610d14a757d8e3f25344c1f375a166
Joseph King <joe.k659@gmail.com>
2024-05-28 11:41:05 -0400

    fix db setup

--- .fly/scripts/1_storage_init.sh
+++
@@ -1,6 +0,0 @@
-FOLDER=/var/www/html/storage/database
-if [ ! -d "$FOLDER" ]; then
-    echo "$FOLDER is not a directory, initializing database"
-    mkdir /var/www/html/storage/database
-    touch /var/www/html/storage/database/database.sqlite
-fi
